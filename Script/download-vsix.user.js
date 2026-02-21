f76ee72e37daf22e3729b1ee6d13cec7da21431c
LuanJian <luanjian@live.cn>
2026-02-21 17:59:11 +0800

    Fix namespace URL formatting in user script

--- Script/download-vsix.user.js
+++ Script/download-vsix.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VS Code 软件离线插件下载
-// @namespace    https://raw.githubusercontent.com/LuanJian/Script/refs/heads/main/Script/download-vsx.user.js
+// @namespace    https://raw.githubusercontent.com/LuanJian/Script/refs/heads/main/Script/download-vsix.user.js
 // @version      1.1
 // @description  在 VS Marketplace 插件页添加下载按钮，直接解析下载 .vsix 文件或跳转到 Open-VSX 插件页面下载。
 // @author       亦木
